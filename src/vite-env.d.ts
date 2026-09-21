d940b085ed63ea353c0e3640fac77406b9729ca1
cameronthreadgill3 <cameronthreadgill3@gmail.com>
2026-09-20 21:55:22 -0500

    feat: safe zone, bank, backpack/weight, death loot, Premium Backpack (part 1 docs+api)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_CLERK_PUBLISHABLE_KEY?: string;
+  readonly VITE_STRIPE_PUBLISHABLE_KEY?: string;
+  readonly VITE_PREMIUM_BACKPACK_PRICE_ID?: string;
+  readonly VITE_PREMIUM_CHECKOUT_DEMO?: string;
 }
 
 interface ImportMeta {
